49be20c98c9e32143dd152ba42530a6600953aad
Glenn Pratt <glenn.pratt@acquia.com>
2018-03-09 15:16:48 -0800

    Build tooling test.

---
+++ acquia/build.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+require 'bundler/setup'
+require 'fileutils'
+
+def shell(cmd)
+  puts cmd
+  fail "#{cmd} failed" unless system(cmd)
+end
+
+name = 'acquiadrush'
+
+
+# TODO Get all this from the current Git tag instead.
+major_ver = '9'
+version = '9.1.0-acquia1'
+
+file = version
+Dir.chdir('..') do
+  shell("composer install --no-dev --no-plugins --no-scripts")
+end
+
+cmd = 'fpm'
+# dir in, deb out
+cmd << ' -s dir -t deb'
+# change dir so files are not nested in 'drush-x.x.x' dir.
+# cmd << " -C #{file}"
+file = '.'
+
+cmd << ' --architecture all'
+cmd << " --name #{name}#{major_ver}"
+cmd << " --version #{version}"
+cmd << " --vendor 'Acquia, Inc.'"
+cmd << ' --license GPL-2.0+'
+cmd << ' --url https://github.com/acquia/fields'
+cmd << " --prefix /usr/local/drush#{major_ver}"
+cmd << " #{file}"
+shell(cmd)
